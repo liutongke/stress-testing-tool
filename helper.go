ec6415963f2eb5fbc3394d1e4b474802b80f68f6
keke <liutongke@live.com>
2022-09-09 17:07:08 +0800

    init

---
+++ helper.go
@@ -0,0 +1,23 @@
+package main
+
+// Package helper 帮助函数，时间、数组的通用处理
+import (
+	"time"
+)
+
+// DiffNano 时间差，纳秒
+func DiffNano(startTime time.Time) (diff time.Duration) {
+	diff = time.Since(startTime)
+	return
+}
+
+// InArrayStr 判断字符串是否在数组内
+func InArrayStr(str string, arr []string) (inArray bool) {
+	for _, s := range arr {
+		if s == str {
+			inArray = true
+			break
+		}
+	}
+	return
+}
